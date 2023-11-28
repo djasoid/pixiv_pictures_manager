bc904afffd7863e420d01886d00a0bfd18e25407
djasoid <zwpgk964jh@qq.com>
2023-11-27 23:15:54 -0800

    tested some functions

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 # main process of the program (with UI)
+import classes
+import pic_data_functions as dataFn
+import pic_file_functions as fileFn
+import program_functions as progFn
+
+
+
+
+# from cfg
+picDataPath = "D:/Pixiv_Pictures/pixiv_picture_data"
+picFilePath = "D:/Pixiv_Pictures/pixiv"
+
+tagTree = dataFn.loadTagTree()
+
+
+metadataDict = fileFn.getAllData(picFilePath)
+dataFn.writeJson(metadataDict, picDataPath, "Metadata.json")
